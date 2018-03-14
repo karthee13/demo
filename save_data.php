1f54e8ab2e4717526d99b9c48e354a7034bfcaf4
karthee13 <karthee13@gmail.com>
2018-03-14 17:47:02 +0530

    commit all

---
+++ save_data.php
@@ -0,0 +1,16 @@
+<?php require_once('global/config.php');
+
+if(!empty($_POST)){
+	unset($_POST['SAVE']);
+	if($_GET['id'] > 0){
+		//db_perform('REGISTRATION_FORM', $_POST, 'update', 'ID = "'.$_GET['id'].'"');
+	}else{
+		$sql="INSERT INTO REGISTRATION_FORM(NAME)VALUES('".mysql_real_escape_string($_POST['NAME'])."')";
+		$result=mysql_query($sql);
+		$id = mysql_insert_id();
+		echo $id;
+	}
+}
+
+
+?>
